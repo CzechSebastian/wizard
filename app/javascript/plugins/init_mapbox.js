3b1e8b45f2f1602604131024f7d67208a5a71224
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-05-28 11:14:53 -0400

    add map and markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,6 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -7,9 +9,29 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-73.61, 45.551],
+      zoom: 9.5,
+      attributionControl: false
     });
+      map.addControl(new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        marker: {
+        },
+        mapboxgl: mapboxgl
+      }));
+
+  const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+  });
+
+
   }
 };
+
+
 
 export { initMapbox };
