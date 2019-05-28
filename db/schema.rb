3564a5bc52dde6aa5607a90548a214667fa694b3
philippe desjardins <phil_94@mac.com>
2019-05-28 14:00:26 -0400

    added a testing table for district

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_175711) do
+ActiveRecord::Schema.define(version: 2019_05_28_174225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "districts", force: :cascade do |t|
+    t.string "name"
+    t.float "score"
+    t.jsonb "coordinates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
