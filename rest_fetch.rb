11f74b4fd2fdedd7a3e57ad9803dddff9db5c6ac
philippe desjardins <phil_94@mac.com>
2019-05-29 10:00:40 -0400

    updated rest_fetch

--- rest_fetch.rb
+++ rest_fetch.rb
@@ -1,7 +1,7 @@
 require "json"
 require "rest-client"
 
-response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=45.4831706,-73.6701374&radius=10000&type=restaurant&key=AIzaSyDuAEacEDldR2S-gfL2d5JWMGYzhl28G4g"
+response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=45.523791,-73.599567&radius=1000&type=restaurant&key=AIzaSyDuAEacEDldR2S-gfL2d5JWMGYzhl28G4g"
 results = JSON.parse(response)
 
 last_page_token = results["next_page_token"]
@@ -12,13 +12,10 @@ restaurants << results["results"]
 
 
 while last_page_token != nil
-	sleep 5
+	sleep 2
 
 	response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDuAEacEDldR2S-gfL2d5JWMGYzhl28G4g&pagetoken=#{last_page_token}"
 	results = JSON.parse(response)
-
-	puts "========================================================================================================="
-	p results
 
 	last_page_token = results["next_page_token"]
 
@@ -27,7 +24,10 @@ end
 
 json_string = JSON.pretty_generate(restaurants) 
 
-File.write("#{Time.now.to_i}_restaurant_seed.json", json_string)
+p restaurants[0]["name"].count
+
+# File.write("#{Time.now.to_i}_restaurant_seed.json", json_string)
+
 
 
 
