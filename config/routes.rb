d0d83dd4c78be321ea4ff2136dc3aca7c460d019
Sebastian Czech <czechsebastian@icloud.com>
2019-05-30 15:45:28 -0400

    toogle button to sample  restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :new ]
-  resources :pages, only: [:index]
   root to: 'users#index'
   resources :maps, only: [:index]
 end
