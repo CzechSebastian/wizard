2eb0f8de121c4cf0ba6f0d17636f993f9349838f
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-05-31 15:36:17 -0400

    skip_before_action in show

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,4 +1,5 @@
 class DistrictsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def show
   	@district = District.find(params[:id])
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!
 
   def index
     @criteria = params[:criteria][1..-1]
