a36c03582e254be0c67c6057d9c39eb61fb627a7
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-05-31 10:56:52 -0400

    add parc to seed and default value to migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,124 +35,124 @@ end
 
 # THIS IS THE CODE FOR RESTAURANT SCORE
 
-# def set_restaurants_score(district)
-#   arr_location = district.location
-#   url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{arr_location[1]},#{arr_location[0]}&radius=1000&type=restaurant&key=#{ENV["GOOGLE_API_KEY"]}"
-#   response = RestClient.get url
-#   results = JSON.parse(response)
+def set_restaurants_score(district)
+  arr_location = district.location
+  url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{arr_location[1]},#{arr_location[0]}&radius=1000&type=restaurant&key=#{ENV["GOOGLE_API_KEY"]}"
+  response = RestClient.get url
+  results = JSON.parse(response)
 
-#   last_page_token = results["next_page_token"]
+  last_page_token = results["next_page_token"]
 
-#   restaurants = []
+  restaurants = []
 
-#   restaurants << results["results"]
+  restaurants << results["results"]
 
-#   while last_page_token != nil
-#     sleep 2
+  while last_page_token != nil
+    sleep 2
 
-#     response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV["GOOGLE_API_KEY"]}&pagetoken=#{last_page_token}"
-#     results = JSON.parse(response)
+    response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV["GOOGLE_API_KEY"]}&pagetoken=#{last_page_token}"
+    results = JSON.parse(response)
 
-#     last_page_token = results["next_page_token"]
+    last_page_token = results["next_page_token"]
 
-#     restaurants << results["results"]
-#   end
+    restaurants << results["results"]
+  end
 
-# 	  puts district.name
-# 	  valid_restaurants = restaurants.flatten.select do |restaurant|
-# 	    !restaurant["rating"].nil? && district.contains_point?([(restaurant["geometry"]["location"]["lng"]).to_f,(restaurant["geometry"]["location"]["lat"]).to_f])
-#   	end
-# 	 if valid_restaurants.length != 0
-#     district.update(raw_restaurant: valid_restaurants)
+	  puts district.name
+	  valid_restaurants = restaurants.flatten.select do |restaurant|
+	    !restaurant["rating"].nil? && district.contains_point?([(restaurant["geometry"]["location"]["lng"]).to_f,(restaurant["geometry"]["location"]["lat"]).to_f])
+  	end
+	 if valid_restaurants.length != 0
+    district.update(raw_restaurant: valid_restaurants)
 
-# 	  number = valid_restaurants.count
-# 	  puts district.name
-# 	  puts number
+	  number = valid_restaurants.count
+	  puts district.name
+	  puts number
 
-# 	  sum = 0
-# 	  valid_restaurants.each do |restaurant|
-# 	    sum += restaurant["rating"]
-# 	  end
+	  sum = 0
+	  valid_restaurants.each do |restaurant|
+	    sum += restaurant["rating"]
+	  end
 
-# 	  average = sum/number
-# 	  puts average
-# 	  district.update(restaurant_score: average)
+	  average = sum/number
+	  puts average
+	  district.update(restaurant_score: average)
 
-# 	else
-# 		puts "This one has 0 places"
-# 		average = 0
-# 		district.update(restaurant_score: average)
-#   end
-# end
+	else
+		puts "This one has 0 places"
+		average = 0
+		district.update(restaurant_score: average)
+  end
+end
 
-#  District.all.each do |district|
-#   set_restaurants_score(district)
-#  end
+ District.all.each do |district|
+  set_restaurants_score(district)
+ end
 
-# "=============================================================================================================================================================================================================================="
-
-
-# "=============================================================================================================================================================================================================================="
-
-#  # THIS IS THE CODE FOR SCHOOL SCORE
-
-# def set_schools_score(district)
-#   arr_location = district.location
-#   url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{arr_location[1]},#{arr_location[0]}&radius=1000&type=school&key=#{ENV["GOOGLE_API_KEY"]}"
-#   response = RestClient.get url
-#   results = JSON.parse(response)
-
-#   last_page_token = results["next_page_token"]
-
-#   schools = []
-
-#   schools << results["results"]
-
-#   while last_page_token != nil
-#     sleep 2
-
-#     response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV["GOOGLE_API_KEY"]}&pagetoken=#{last_page_token}"
-#     results = JSON.parse(response)
-
-#     last_page_token = results["next_page_token"]
-
-#     schools << results["results"]
-#   end
-
-#     valid_schools = schools.flatten.select do |school|
-#       !school["rating"].nil? && district.contains_point?([(school["geometry"]["location"]["lng"]).to_f,(school["geometry"]["location"]["lat"]).to_f])
-#     end
-#    if valid_schools.length != 0
-#     district.update(school_raw: valid_schools)
-
-#     number = valid_schools.count
-#     puts district.name
-#     puts number
-
-#     sum = 0
-#     valid_schools.each do |school|
-#       sum += school["rating"]
-#     end
-
-#     average = sum/number
-#     puts average
-#     district.update(school_score: average)
-
-#   else
-#     puts "This one has 0 places"
-#     average = 0
-#     district.update(school_score: average)
-#   end
-# end
-
-#  District.all.each do |district|
-#   set_schools_score(district)
-#  end
-
-# "=============================================================================================================================================================================================================================="
+"=============================================================================================================================================================================================================================="
 
 
-# "=============================================================================================================================================================================================================================="
+"=============================================================================================================================================================================================================================="
+
+ # THIS IS THE CODE FOR SCHOOL SCORE
+
+def set_schools_score(district)
+  arr_location = district.location
+  url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{arr_location[1]},#{arr_location[0]}&radius=1000&type=school&key=#{ENV["GOOGLE_API_KEY"]}"
+  response = RestClient.get url
+  results = JSON.parse(response)
+
+  last_page_token = results["next_page_token"]
+
+  schools = []
+
+  schools << results["results"]
+
+  while last_page_token != nil
+    sleep 2
+
+    response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV["GOOGLE_API_KEY"]}&pagetoken=#{last_page_token}"
+    results = JSON.parse(response)
+
+    last_page_token = results["next_page_token"]
+
+    schools << results["results"]
+  end
+
+    valid_schools = schools.flatten.select do |school|
+      !school["rating"].nil? && district.contains_point?([(school["geometry"]["location"]["lng"]).to_f,(school["geometry"]["location"]["lat"]).to_f])
+    end
+   if valid_schools.length != 0
+    district.update(school_raw: valid_schools)
+
+    number = valid_schools.count
+    puts district.name
+    puts number
+
+    sum = 0
+    valid_schools.each do |school|
+      sum += school["rating"]
+    end
+
+    average = sum/number
+    puts average
+    district.update(school_score: average)
+
+  else
+    puts "This one has 0 places"
+    average = 0
+    district.update(school_score: average)
+  end
+end
+
+ District.all.each do |district|
+  set_schools_score(district)
+ end
+
+"=============================================================================================================================================================================================================================="
+
+
+"=============================================================================================================================================================================================================================="
 
 #  # THIS IS THE CODE FOR SUBWAY SCORE
 
@@ -210,63 +210,63 @@ end
 #   set_subways_score(district)
 # end
 
-"=============================================================================================================================================================================================================================="
+# "=============================================================================================================================================================================================================================="
 
 
-"=============================================================================================================================================================================================================================="
+# "=============================================================================================================================================================================================================================="
 
 # THIS IS THE CODE FOR RESTAURANT SCORE
 
-def set_parks_score(district)
-  arr_location = district.location
-  url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{arr_location[1]},#{arr_location[0]}&radius=1000&type=park&key=#{ENV["GOOGLE_API_KEY"]}"
-  response = RestClient.get url
-  results = JSON.parse(response)
+# def set_parks_score(district)
+#   arr_location = district.location
+#   url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{arr_location[1]},#{arr_location[0]}&radius=1000&type=park&key=#{ENV["GOOGLE_API_KEY"]}"
+#   response = RestClient.get url
+#   results = JSON.parse(response)
 
-  last_page_token = results["next_page_token"]
+#   last_page_token = results["next_page_token"]
 
-  parks = []
+#   parks = []
 
-  parks << results["results"]
+#   parks << results["results"]
 
-  while last_page_token != nil
-    sleep 2
+#   while last_page_token != nil
+#     sleep 2
 
-    response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV["GOOGLE_API_KEY"]}&pagetoken=#{last_page_token}"
-    results = JSON.parse(response)
+#     response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV["GOOGLE_API_KEY"]}&pagetoken=#{last_page_token}"
+#     results = JSON.parse(response)
 
-    last_page_token = results["next_page_token"]
+#     last_page_token = results["next_page_token"]
 
-    parks << results["results"]
-  end
+#     parks << results["results"]
+#   end
 
-    valid_parks = parks.flatten.select do |park|
-      !park["rating"].nil? && district.contains_point?([(park["geometry"]["location"]["lng"]).to_f,(park["geometry"]["location"]["lat"]).to_f])
-    end
-   if valid_parks.length != 0
-    district.update(park_raw: valid_parks)
+#     valid_parks = parks.flatten.select do |park|
+#       !park["rating"].nil? && district.contains_point?([(park["geometry"]["location"]["lng"]).to_f,(park["geometry"]["location"]["lat"]).to_f])
+#     end
+#    if valid_parks.length != 0
+#     district.update(park_raw: valid_parks)
 
-    number = valid_parks.count
-    puts district.name
-    puts number
+#     number = valid_parks.count
+#     puts district.name
+#     puts number
 
-    sum = 0
-    valid_parks.each do |park|
-      sum += park["rating"]
-    end
+#     sum = 0
+#     valid_parks.each do |park|
+#       sum += park["rating"]
+#     end
 
-    average = sum/number
-    puts average
-    district.update(park_score: average)
+#     average = sum/number
+#     puts average
+#     district.update(park_score: average)
 
-  else
-    puts "This one has 0 places"
-    average = 0
-    district.update(park_score: average)
-  end
-end
+#   else
+#     puts "This one has 0 places"
+#     average = 0
+#     district.update(park_score: average)
+#   end
+# end
 
- District.all.each do |district|
-  set_parks_score(district)
- end
+#  District.all.each do |district|
+#   set_parks_score(district)
+#  end
 
