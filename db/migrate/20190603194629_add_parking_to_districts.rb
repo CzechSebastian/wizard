23f7e5d47935078c4891a8d2ffe05908a3e55d86
Sebastian Czech <czechsebastian@icloud.com>
2019-06-03 17:55:08 -0400

    fixed

--- db/migrate/20190603194629_add_parking_to_districts.rb
+++ db/migrate/20190603194629_add_parking_to_districts.rb
@@ -1,4 +1,4 @@
-class AddCarToDistricts < ActiveRecord::Migration[5.2]
+class AddParkingToDistricts < ActiveRecord::Migration[5.2]
   def change
     add_column :districts, :parking_score, :float, default: 0.0
     add_column :districts, :parking_raw, :jsonb, default: []
