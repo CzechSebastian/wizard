5155dbed5be542a2d258010325d1a6010aaa000c
Sebastian Czech <czechsebastian@icloud.com>
2019-06-03 10:06:11 -0400

    start landing page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,19 @@ $(document).ready(function(){
     }, 100)
   });
 });
+
+//Button to top
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
