f0da00806e9870c565d81b3836cc93c71520cfd2
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-06-06 13:34:00 -0400

    add reviews and summary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_145648) do
+ActiveRecord::Schema.define(version: 2019_06_06_163422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 2019_06_04_145648) do
     t.jsonb "cafe_raw", default: []
     t.float "gym_score", default: 0.0
     t.jsonb "gym_raw", default: []
+    t.string "language", default: "french"
+    t.string "crime_rate", default: "low"
+    t.integer "age", default: 30
+    t.integer "rent_rate", default: 900
   end
 
   create_table "users", force: :cascade do |t|
